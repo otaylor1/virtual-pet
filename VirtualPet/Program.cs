09b93d51422c3243bd62d2216ae70adf356e766b
Olivia Taylor <obt1@pitt.edu>
2016-06-24 12:51:32 -0400

    Name pet menu

---
+++ VirtualPet/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtualPet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("You have just adopted a new Sloth!");
+            Console.Write("What is its name? ");
+            string name = Console.ReadLine();
+            Console.WriteLine(name + " is happy to meet you!");
+            Console.ReadKey();
+            
+
+
+
+
+
+
+                
+        }
+    }
+}
